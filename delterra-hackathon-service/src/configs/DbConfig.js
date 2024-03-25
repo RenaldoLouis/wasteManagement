b995c01be78a4735bb8523b53b95eef8820ecea9
Renaldo Louis <renaldo.louis@commsult.id>
2024-03-25 19:28:17 +0700

    add user in dbCOnfig

--- delterra-hackathon-service/src/configs/DbConfig.js
+++ delterra-hackathon-service/src/configs/DbConfig.js
@@ -17,6 +17,7 @@ require('dotenv').config()
 const db = {
   // user: 'renaldolouis',
   host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.PORT,
