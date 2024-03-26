7824a75fcde9db9e0a696071b655f3fac57cd2b4
Renaldo Louis <renaldo.louis@commsult.id>
2024-03-26 18:00:18 +0700

    fix link

--- delterra-hackathon-spa/src/Constants/Api.js
+++ delterra-hackathon-spa/src/Constants/Api.js
@@ -2,7 +2,7 @@
 // export const USER_API = "apps";
 // export const BASE_URL = "https://pokeapi.co/api/v2/";
 // export const USER_API = "pokemon/";
-export const BASE_URL = "http://192.168.210.99:3001/api/v1/delterra/";
-export const INBOUND_API = "inboundDelivery";
 export const WASTE_API = "waste"
+export const BASE_URL = "delterra-02.commsult.dev/api/v1/delterra/";
+export const INBOUND_API = "inboundDelivery";
 export const DASHBOARD_API = "waste";
