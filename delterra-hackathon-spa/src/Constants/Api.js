5debaa1ed372255cb2ce943c128479dd0245c7ad
Vernando Cahyadi <vernando.cahyadi@commsult.id>
2024-03-26 12:39:58 +0000

    Api.js edited online with Bitbucket

--- delterra-hackathon-spa/src/Constants/Api.js
+++ delterra-hackathon-spa/src/Constants/Api.js
@@ -3,6 +3,6 @@
 // export const BASE_URL = "https://pokeapi.co/api/v2/";
 // export const USER_API = "pokemon/";
 export const WASTE_API = "waste"
-export const BASE_URL = "delterra-02.commsult.dev/api/v1/delterra/";
+export const BASE_URL = "https://delterra-02.commsult.dev/api/v1/delterra/";
 export const INBOUND_API = "inboundDelivery";
 export const DASHBOARD_API = "waste";
